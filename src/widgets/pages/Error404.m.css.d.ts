b1e137c2c7f6a41f8ffc924459fe9f6486615327
Tom Dye <github@tomdye.co.uk>
2020-06-09 15:33:38 +0100

    fix 404 page

---
+++ src/widgets/pages/Error404.m.css.d.ts
@@ -0,0 +1,1 @@
+export const heroWrapper: string;
--- src/widgets/pages/Error404.tsx
+++ src/widgets/pages/Error404.tsx
@@ -1,6 +1,7 @@
 import { create, tsx } from '@dojo/framework/core/vdom';
 import { Hero } from '../Hero';
 import * as commonCss from '../../Common.m.css';
+import * as css from './Error404.m.css';
 
 export interface Error404Properties {}
 
@@ -8,12 +9,17 @@ const factory = create().properties<Error404Properties>();
 
 export const Error404 = factory(function Error404() {
 	return (
-		<div classes={commonCss.contentWrapper} styles={{ background: 'blue' }}>
-			<Hero>
-				{{
-					xlTitle: `Error404`
-				}}
-			</Hero>
+		<div classes={css.heroWrapper}>
+			<div classes={commonCss.contentWrapper}>
+				<Hero>
+					{{
+						title: 'Error 404',
+						xlTitle: `PAGE NOT FOUND`,
+						blurb:
+							'The link you clicked may be broken or the page may have been removed.'
+					}}
+				</Hero>
+			</div>
 		</div>
 	);
 });
