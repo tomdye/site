1d3d684fdc8c85a625306b1f1a72dd3103711b2a
Tom Dye <github@tomdye.co.uk>
2020-06-09 11:39:08 +0100

    combine and tidy

--- src/widgets/GDPRBanner.tsx
+++ src/widgets/GDPRBanner.tsx
@@ -6,10 +6,10 @@ const factory = create();
 
 export const GDPRBanner = factory(function GDPRBanner() {
 	return (
-		<div id="gdprbanner" classes={[css.root, 'hidden']}>
+		<div id="gdpr-banner" classes={[css.root, 'hidden']}>
 			We use cookies to ensure that we give you the best experience on our website. If you
 			continue to use this site we will assume that you are happy with it.
-			<button id="gdpraccept" type="button" classes={css.button}>
+			<button id="gdpr-accept" type="button" classes={css.button}>
 				OK
 			</button>
 			<Link classes={css.button} to="privacy">
